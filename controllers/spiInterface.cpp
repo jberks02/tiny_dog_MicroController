e94929983734615c80aaf27b58a0daa2c3fc3c1a
Justin Berkshire <justinwendellberkshire@gmail.com>
2023-04-24 00:35:03 -0500

    saving changes to prevent accidental loss

--- controllers/spiInterface.cpp
+++ controllers/spiInterface.cpp
@@ -5,7 +5,8 @@ class SpiInterface
 {
 private:
     string write = "";
-
+    string read = "";
+    bool writeNeedsConstruct = true;
 public:
     SpiInterface(uint tx = 19, uint sck = 18, uint csn = 17, uint rx = 16, uint baudrate = 1000*100, uint CPOL = 0, uint CPHA = 0)
     {
@@ -30,41 +31,41 @@ public:
     {
         spi_set_slave(spi0, true);
     }
-    void spiDynamicExchange()
-    {
+    // void spiDynamicExchange()
+    // {
 
-        string read;
+    //     string read;
 
-        // if(charIndex >= static_cast<int>(write.size())) {
-        //     charIndex = 0;
-        //     construct_new_write();
-        // }
+    //     // if(charIndex >= static_cast<int>(write.size())) {
+    //     //     charIndex = 0;
+    //     //     construct_new_write();
+    //     // }
 
-        construct_new_write();
+    //     construct_new_write();
 
-        char tx[write.size()];
-        strcpy(tx, write.c_str());
+    //     char tx[write.size()];
+    //     strcpy(tx, write.c_str());
 
-        char lastByteRead = 0x00;
-        int charIndex = 0;
+    //     char lastByteRead = 0x00;
+    //     int charIndex = 0;
 
-        while (lastByteRead != 0x03)
-        {
-            uint8_t nextWrite[1];
-            nextWrite[0] = sizeof(tx) <= charIndex ? (uint8_t)tx[charIndex] : 0x00;
-            uint8_t rx[1];
+    //     while (lastByteRead != 0x03)
+    //     {
+    //         uint8_t nextWrite[1];
+    //         nextWrite[0] = sizeof(tx) <= charIndex ? (uint8_t)tx[charIndex] : 0x00;
+    //         uint8_t rx[1];
 
-            spi_write_read_blocking(spi0, nextWrite, rx, 1);
-            lastByteRead = rx[1];
-            if (rx[1] > 0x20)
-            {
-                read.push_back(static_cast<char>(rx[1]));
-            }
-            charIndex = charIndex + 1;
-        }
+    //         spi_write_read_blocking(spi0, nextWrite, rx, 1);
+    //         lastByteRead = rx[1];
+    //         if (rx[1] > 0x20)
+    //         {
+    //             read.push_back(static_cast<char>(rx[1]));
+    //         }
+    //         charIndex = charIndex + 1;
+    //     }
 
-        process_command(read, read.size());
-    }
+    //     process_command(read, read.size());
+    // }
     // uint exchangeByteMessage()
     // {
     //     try
@@ -130,6 +131,9 @@ private:
             write.swap(loadedWrite);
 
             pause_updates = false;
+
+            writeNeedsConstruct = false;
+
         }
         catch (...)
         {
@@ -139,87 +143,129 @@ private:
     }
 
 public:
-    void readOutSpiBuffer() {
+    void transfer32Bytes() {
+        try {
 
-        if(spi_is_readable(spi0) == false) return;
-    
-        vector<char> dynamicRead;
-        
-        while (spi_is_readable(spi0))
-        {
-            uint8_t rx[1] = {0x00};
-            spi_read_blocking(spi0, {0x00}, rx, 1);
-            if (rx[0] > 0x14)
-            {
-                dynamicRead.push_back(rx[0]);
+            if (spi_is_readable(spi0) == false) return;
+            if(writeNeedsConstruct == true) {
+                construct_new_write();
+            };
+
+            char read_ar[32];
+            char write_ar[32];
+
+            memset(read_ar, 0x00, 32);
+            memset(write_ar, 0x00, 32);
+
+            for(int i = 0; i < 32; i++) {
+                if(write.length() > 0) {
+                    write_ar[i] = write[0];
+                    write.erase(write.begin());
+                } else {
+                    writeNeedsConstruct = true;
+                }
             }
+
+            spi_write_read_blocking(spi0, (uint8_t *)write_ar, (uint8_t *)read_ar, 32);
+
+            bool readComplete = false; 
+
+            read = read + string(read_ar);
+
+            for(int i = 0; i < 32; i++) {
+                if(read_ar[i] == 0x03) readComplete = true;
+            };
+
+            if(readComplete == true) {
+                process_command(read, read.size());
+                read.clear();
+            }
+
+        } catch(...) {
+            cout << "Failure to transfer 32 Bytes" << endl;
         }
-
-        string readBytes(dynamicRead.data(), dynamicRead.size());
-
-        process_command(readBytes, readBytes.size());
-
     }
-    void writeOutLatestStatement() {
+    // void readOutSpiBuffer() {
 
-        if(spi_is_writable(spi0) == false) return;
+    //     if(spi_is_readable(spi0) == false) return;
+    
+    //     vector<char> dynamicRead;
+        
+    //     while (spi_is_readable(spi0))
+    //     {
+    //         uint8_t rx[1] = {0x00};
+    //         spi_read_blocking(spi0, {0x00}, rx, 1);
+    //         if (rx[0] > 0x14)
+    //         {
+    //             dynamicRead.push_back(rx[0]);
+    //         }
+    //     }
 
-        construct_new_write();
+    //     string readBytes(dynamicRead.data(), dynamicRead.size());
 
-        char write_ar[write.size()];
+    //     process_command(readBytes, readBytes.size());
 
-        memset(write_ar, 0x00, write.size());
+    // }
+    // void writeOutLatestStatement() {
 
-        for (int i = 0; i < write.size(); i++) {
-            write_ar[i] = write[i];
-        };
+    //     if(spi_is_writable(spi0) == false) return;
 
-        spi_write_blocking(spi0, (uint8_t *)write_ar, sizeof(write_ar));
+    //     construct_new_write();
 
-        loadedWrite.clear();
+    //     char write_ar[write.size()];
 
-    }
-    void fullReadWriteTransfer()
-    {
+    //     memset(write_ar, 0x00, write.size());
 
-        vector<char> dynamicRead; 
-        char tx[6] = {0x02, 'P', 'i', 'c', 'o', 0x03};
-        bool writeReady = spi_is_writable(spi0);
-        bool readReady = spi_is_readable(spi0);
+    //     for (int i = 0; i < write.size(); i++) {
+    //         write_ar[i] = write[i];
+    //     };
 
-        if(writeReady == true && readReady == false) {
-            spi_write_blocking(spi0, (uint8_t *)tx, sizeof(tx));
-        } else if (writeReady == false && readReady == true) {
-            while(spi_is_readable(spi0)) {
-                uint8_t rx[1] = {0x00};
-                spi_read_blocking(spi0, {0x00} ,rx, 1);
-                if(rx[0] > 0x14) {
-                    dynamicRead.push_back(rx[0]);
-                }
-            }
-        } else if (writeReady && readReady) {
-            int byteIndex = 0;
-            while(spi_is_readable(spi0) && spi_is_writable(spi0)) {
-                uint8_t nextWrite[1] = {0x00};
-                nextWrite[0] = sizeof(tx) > byteIndex ? tx[byteIndex] : 0x00;
-                uint8_t nextRead[1] = {0x00};
-                spi_write_read_blocking(spi0, nextWrite, nextRead, 1);
-                if(nextRead[0] > 0x14) dynamicRead.push_back(nextRead[0]);
-                byteIndex = byteIndex + 1;
-            }
-            if(byteIndex < sizeof(tx) && spi_is_writable(spi0)) {
-                uint remainingWriteLength = sizeof(tx) - byteIndex;
-                uint8_t remainingWrite[remainingWriteLength];
-                for(int i = 0; i < remainingWriteLength; i++) {
-                    remainingWrite[i] = tx[i + remainingWriteLength];
-                }
-                spi_write_blocking(spi0, remainingWrite, remainingWriteLength);
-            }
-        } else return;
+    //     spi_write_blocking(spi0, (uint8_t *)write_ar, sizeof(write_ar));
 
-        string readBytes(dynamicRead.data(), dynamicRead.size());
+    //     loadedWrite.clear();
 
-        process_command(readBytes, readBytes.size());
+    // }
+    // void fullReadWriteTransfer()
+    // {
 
-    }
+    //     vector<char> dynamicRead; 
+    //     char tx[6] = {0x02, 'P', 'i', 'c', 'o', 0x03};
+    //     bool writeReady = spi_is_writable(spi0);
+    //     bool readReady = spi_is_readable(spi0);
+
+    //     if(writeReady == true && readReady == false) {
+    //         spi_write_blocking(spi0, (uint8_t *)tx, sizeof(tx));
+    //     } else if (writeReady == false && readReady == true) {
+    //         while(spi_is_readable(spi0)) {
+    //             uint8_t rx[1] = {0x00};
+    //             spi_read_blocking(spi0, {0x00} ,rx, 1);
+    //             if(rx[0] > 0x14) {
+    //                 dynamicRead.push_back(rx[0]);
+    //             }
+    //         }
+    //     } else if (writeReady && readReady) {
+    //         int byteIndex = 0;
+    //         while(spi_is_readable(spi0) && spi_is_writable(spi0)) {
+    //             uint8_t nextWrite[1] = {0x00};
+    //             nextWrite[0] = sizeof(tx) > byteIndex ? tx[byteIndex] : 0x00;
+    //             uint8_t nextRead[1] = {0x00};
+    //             spi_write_read_blocking(spi0, nextWrite, nextRead, 1);
+    //             if(nextRead[0] > 0x14) dynamicRead.push_back(nextRead[0]);
+    //             byteIndex = byteIndex + 1;
+    //         }
+    //         if(byteIndex < sizeof(tx) && spi_is_writable(spi0)) {
+    //             uint remainingWriteLength = sizeof(tx) - byteIndex;
+    //             uint8_t remainingWrite[remainingWriteLength];
+    //             for(int i = 0; i < remainingWriteLength; i++) {
+    //                 remainingWrite[i] = tx[i + remainingWriteLength];
+    //             }
+    //             spi_write_blocking(spi0, remainingWrite, remainingWriteLength);
+    //         }
+    //     } else return;
+
+    //     string readBytes(dynamicRead.data(), dynamicRead.size());
+
+    //     process_command(readBytes, readBytes.size());
+
+    // }
 };
--- main.cpp
+++ main.cpp
@@ -21,26 +21,26 @@ int main()
 
     controller.setServoController(&ServoController);
 
-    vector<vector<float>> yzPlane = {{86.61356, -33.f}, {86.61356, 0.f}, {0.f, 0.f}};
-    vector<vector<float>> xyPlane = {{0.f, 86.61356}, {-80.f, 86.61356}, {0, 0.f}};
-    vector<float> endaffectorCoordinate = {0.f, 0.f, 0.f};
-    vector<PositioningServo> servos = {PositioningServo(0, 'r', 90.f, {0.f, 86.61356, 0.f}), PositioningServo(1, 'l', 32.f, {-80.f, 86.61356, 0.f}, "quadratic"), PositioningServo(2, 'r', 90.f, {0.f, 86.61356, -33.f}, "default", true)}; // {0.f, 110.f, 0.f}, {0.f, 110.f, -33.f}
-    ExtensionTracker rfLeg("rightFront", yzPlane, xyPlane, endaffectorCoordinate, servos);
-    controller.setNewExtensionTracker(rfLeg);
-    MovementSeries test("test", "transition", 20, {{0.f, 30.f, 0.f}, {90.f, -20.f, -60.f}, {-60.f, -20.f, 60.f}}); //{0.f,20.f,0.f},
-    MovementSeries test2("test2", "transition", 20, {{0.f, 30.f, 0.f}, {90.f, -20.f, 0.f}, {-60.f, -20.f, 0.f}});  //{0.f,20.f,0.f},
-    test.increaseResolution(5);
-    test2.increaseResolution(5);
-    controller.setNewMovementSeriesForExtension("rightFront", test);
-    controller.setNewMovementSeriesForExtension("rightFront", test2);
-    extensionSeriesCommand series("rightFront", "test2", 3);
-    extensionSeriesCommand series2("rightFront", "test", 4);
-    controller.prepareNextSeries(series);
-    controller.prepareNextSeries(series2);
+    // vector<vector<float>> yzPlane = {{86.61356, -33.f}, {86.61356, 0.f}, {0.f, 0.f}};
+    // vector<vector<float>> xyPlane = {{0.f, 86.61356}, {-80.f, 86.61356}, {0, 0.f}};
+    // vector<float> endaffectorCoordinate = {0.f, 0.f, 0.f};
+    // vector<PositioningServo> servos = {PositioningServo(0, 'r', 90.f, {0.f, 86.61356, 0.f}), PositioningServo(1, 'l', 32.f, {-80.f, 86.61356, 0.f}, "quadratic"), PositioningServo(2, 'r', 90.f, {0.f, 86.61356, -33.f}, "default", true)}; // {0.f, 110.f, 0.f}, {0.f, 110.f, -33.f}
+    // ExtensionTracker rfLeg("rightFront", yzPlane, xyPlane, endaffectorCoordinate, servos);
+    // controller.setNewExtensionTracker(rfLeg);
+    // MovementSeries test("test", "transition", 20, {{0.f, 30.f, 0.f}, {90.f, -20.f, -60.f}, {-60.f, -20.f, 60.f}}); //{0.f,20.f,0.f},
+    // MovementSeries test2("test2", "transition", 20, {{0.f, 30.f, 0.f}, {90.f, -20.f, 0.f}, {-60.f, -20.f, 0.f}});  //{0.f,20.f,0.f},
+    // test.increaseResolution(5);
+    // test2.increaseResolution(5);
+    // controller.setNewMovementSeriesForExtension("rightFront", test);
+    // controller.setNewMovementSeriesForExtension("rightFront", test2);
+    // extensionSeriesCommand series("rightFront", "test2", 3);
+    // extensionSeriesCommand series2("rightFront", "test", 4);
+    // controller.prepareNextSeries(series);
+    // controller.prepareNextSeries(series2);
     // multicore_reset_core1();
     // multicore_launch_core1(splitToSecondCore);
 
-    controller.runExtensions();
+    // controller.runExtensions();
 
     gpio_init(LED);
 
@@ -57,8 +57,8 @@ int main()
     while (true)
     {
         gpio_put(LED, 0);
-        communication.readOutSpiBuffer();
-        communication.writeOutLatestStatement();
+        // communication.readOutSpiBuffer();
+        // communication.writeOutLatestStatement();
         gpio_put(LED, 1);
         if (ExtensionTrackerList.size() > 0)
         {
